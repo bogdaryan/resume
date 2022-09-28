3ec3cb4049bcfa7c47e6ce85dda187d1ec60d71c
Bogdan <bogdan10050@gmail.com>
2022-09-28 15:39:56 +0300

    a

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ const projectRoot = path.resolve(__dirname);
 module.exports = {
     entry: "./src/index.js",
     output: {
-        path: path.join(__dirname, "/dist"),
+        path: path.join(__dirname, "/docs"),
         filename: "index_bundle.js"
     },
     resolve: {
